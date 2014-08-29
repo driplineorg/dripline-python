d70b4f2fe8103d6ad760f410bd14f8158b53121b
Jared Kofron <jared.kofron@gmail.com>
2014-08-28 21:58:44 -0700

    added one test that will currently fail - to fix tomorrow.  config tests are a work in progress.

--- dripline/tests/test_config.py
+++ dripline/tests/test_config.py
@@ -6,6 +6,9 @@ sys.path.insert(0, myPath + '/../')
 import pytest
 from config import Config
 
+# This section tests that a "good" configuration, where "good" means
+# that a sane configuration can be generated from the yaml file,
+# generates a sane instance of Config.
 @pytest.fixture
 def good_conf():
     filename = myPath + '/test_config.yaml'
@@ -28,3 +31,28 @@ def test_config_instrument_1(good_conf):
 def test_config_instrument_2(good_conf):
     instrument = good_conf.instruments['another_box_with_knobs']
     assert instrument['model'] == 'black_magic_13'
+
+# This section tests that various bad configurations return
+# the expected (very informative) errors.
+@pytest.fixture
+def identical_names_conf():
+    """A configuration that has two endpoints with the same name.
+    Trying to generate a Config object from this configuration
+    should produce an error indicating that another endpoint
+    with that name is already defined, and point to the section that
+    it is defined in.
+    """
+    conf = """
+    broker: foo.bar.baz
+    """
+    return conf
+
+@pytest.mark.xfail(raises=ValueError)
+def test_identical_names_is_error(identical_names_conf):
+    """Verify that identically named endpoints are forbidden.
+    Should this be a ValueError for the key name?"""
+    Config.from_string(identical_names_conf)
+
+
+
+
