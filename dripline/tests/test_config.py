616187b764dc6caa03b0bf7ea047942bce7bc546
Jared Kofron <jared.kofron@gmail.com>
2014-08-28 21:39:10 -0700

    cosmetic

--- dripline/tests/test_config.py
+++ dripline/tests/test_config.py
@@ -8,23 +8,23 @@ from config import Config
 
 @pytest.fixture
 def good_conf():
-	filename = myPath + '/test_config.yaml'
-	c = Config(filename)
-	return c 
+    filename = myPath + '/test_config.yaml'
+    c = Config(filename)
+    return c
 
 def test_node_name(good_conf):
-	assert good_conf.nodename == 'baz'
+    assert good_conf.nodename == 'baz'
 
-def test_host_config(good_conf):	
-	assert good_conf.broker == 'foo.bar.org'
+def test_host_config(good_conf):
+    assert good_conf.broker == 'foo.bar.org'
 
 def test_config_has_instruments(good_conf):
-	assert good_conf.instrument_count() == 2
+    assert good_conf.instrument_count() == 2
 
 def test_config_instrument_1(good_conf):
-	instrument = good_conf.instruments['box_with_knobs']
-	assert instrument['model'] == 'black_magic_12'
+    instrument = good_conf.instruments['box_with_knobs']
+    assert instrument['model'] == 'black_magic_12'
 
 def test_config_instrument_2(good_conf):
-	instrument = good_conf.instruments['another_box_with_knobs']
-	assert instrument['model'] == 'black_magic_13'
+    instrument = good_conf.instruments['another_box_with_knobs']
+    assert instrument['model'] == 'black_magic_13'
