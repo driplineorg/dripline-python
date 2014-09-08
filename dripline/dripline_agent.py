93bfcdb4ab1249b4c0e3fe5546a0ffc21ceb60b9
Jared Kofron <jared.kofron@gmail.com>
2014-09-08 12:33:18 -0700

    move verb checking to before Node construction.

--- dripline/dripline_agent.py
+++ dripline/dripline_agent.py
@@ -18,28 +18,39 @@ PARSER.add_argument('verb')
 PARSER.add_argument('target')
 PARSER.add_argument('value')
 
+def verb_list():
+    """
+    A list of acceptable verb arguments.
+    """
+    return ['get', 'set', 'describe']
+
 def main():
     # TODO: we shouldn't have to start an entire node to do this, should we?
     # a connection should suffice...
     args = PARSER.parse_args()
+
+    # TODO: can't we use argparse to do this somehow?
+    # if the request verb is not one of the accepted verbs, barf here.
+    request_verb = args.verb
+    if request_verb not in verb_list():
+        print("ERROR: verb argument must be one of: get, set, or describe.")
+        return -1
+
     conf = Config(args.config)
     node = Node(conf)
 
-    if args.verb == 'get':
+    if request_verb == 'get':
         request = message.RequestMessage(target=args.target,
                                          msgop=constants.OP_SENSOR_GET)
 
         reply = node.send_sync(request)
         print(reply.payload)
-    elif args.verb == 'set':
+    elif request_verb == 'set':
         request = message.RequestMessage(target=args.target,
                                          msgop=constants.OP_SENSOR_SET,
                                          payload=args.value)
         reply = node.send_sync(request)
         print(reply.payload)
-
-    else:
-        print("sorry!  only get is supported for now.")
 
 if __name__ == '__main__':
     main()
