7c165b37103fa9bb2fb0fa817314a515f9e5eb7e
Ben LaRoque <benjamin.laroque@gmail.com>
2014-09-11 14:14:59 -0700

    and a test on the old factory model

--- dripline/tests/old_test_factory.py
+++ dripline/tests/old_test_factory.py
@@ -4,8 +4,8 @@ myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
 
 import pytest
-from agilent34461a import Agilent34461a
-from factory import constructor_registry
+from dripline.instruments.agilent34461a import Agilent34461a
+from ..factory import constructor_registry
 
 def test_reg_factory():
     c = constructor_registry['agilent34461a']
