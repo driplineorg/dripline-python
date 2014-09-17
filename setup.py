cadbd45f0eebb70a834b54da4dc47177e66a4576
Ben LaRoque <benjamin.laroque@gmail.com>
2014-09-17 11:37:17 -0700

    adding scripts to setup

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ from setuptools.command.test import test as TestCommand
 
 verstr = "none"
 try:
-    verstr = open("VERSION").read().strip().replace(' ','.')
+    verstr = open("VERSION").read().strip().replace(' ', '.')
 except EnvironmentError:
     pass
 except:
     raise RuntimeError("unable to find version")
+
 
 class PyTest(TestCommand):
     user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
@@ -28,20 +29,21 @@ class PyTest(TestCommand):
         self.test_suite = True
 
     def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
+        # import here, because outside the eggs aren't loaded
         import pytest
         errno = pytest.main(self.pytest_args)
         sys.exit(errno)
 
 setup(
-    name = 'dripline',
-    version = verstr,
-    packages = ['dripline'],
-    install_requires = ['pika>=0.9.8', 'PyYAML', 'msgpack-python'],
-    extras_require = {
+    name='dripline',
+    version=verstr,
+    packages=['dripline'],
+    scripts=glob('bin/*'),
+    install_requires=['pika>=0.9.8', 'PyYAML', 'msgpack-python'],
+    extras_require={
         'doc': ['sphinx', 'sphinx_rtd_theme'],
     },
-    url = 'http://www.github.com/project8/dripline',
+    url='http://www.github.com/project8/dripline',
     tests_require=['pytest'],
-    cmdclass = {'test': PyTest}
+    cmdclass={'test': PyTest}
 )
