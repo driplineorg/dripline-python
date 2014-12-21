55ebee1f9421f6ec4b97ff6ed139c6c119634bf0
Ben LaRoque <benjamin.laroque@gmail.com>
2014-12-21 12:24:23 -0800

    yes i'm inserting data into postgres now, what of it

--- dripline/core/alert_consumer.py
+++ dripline/core/alert_consumer.py
@@ -19,6 +19,7 @@ class AlertConsumer:
    
     def __init__(self, broker_host='localhost', exchange='alerts', keys=['#']):
         logger.debug('AlertConsumer initializing')
+        self.table = None
         self.dripline_connection = Connection(broker_host=broker_host)
         self.dripline_connection._setup_amqp()
         self.queue = self.dripline_connection.chan.queue_declare(auto_delete=True)
@@ -27,11 +28,25 @@ class AlertConsumer:
                                                      queue=self.queue.method.queue,
                                                      routing_key=key,
                                                     )
+    def this_consume(self, message):
+        raise NotImplementedError('you must set this_consume to a valid function')
 
     @staticmethod
-    def custom_consume(message):
+    def _print_consume(message):
         logger.debug('using default message consumption')
         logger.info('{}'.format(message))
+    def _postgres_consume(self, message):
+        try:
+            value = float(value)
+        except:
+            pass
+        value_key = 'value_raw'
+        insert_dict = {'endpoint_name': message['payload']['from'],
+                       'timestamp': message['timestamp'],
+                       'value_raw': message['payload']['value'],
+                      }
+        ins = self.table.insert().values(**insert_dict)
+        ins.execute()
 
     def start(self):
         logger.debug("AlertConsmer consume starting")
@@ -39,7 +54,7 @@ class AlertConsumer:
             logger.debug('in process_message callback')
             message_unpacked = Message.from_msgpack(message)
             message_unpacked.payload = msgpack.unpackb(message_unpacked.payload)
-            self.custom_consume(message_unpacked)
+            self.this_consume(message_unpacked)
         self.dripline_connection.chan.basic_consume(process_message,
                                                      queue=self.queue.method.queue,
                                                      no_ack=True
