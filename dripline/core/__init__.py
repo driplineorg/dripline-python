ce8b4f5ba36c54abfb96e01475f266eb041de2fd
Ben LaRoque <benjamin.laroque@gmail.com>
2015-01-07 17:13:18 -0800

    and in __init__

--- dripline/core/__init__.py
+++ dripline/core/__init__.py
@@ -7,6 +7,7 @@ The core namespace contains the layers of abstraction over AMQP.
 from __future__ import absolute_import
 
 from .alert_consumer import *
+from .arg_parser import *
 from .binding import *
 from .config import *
 from .constants import *
