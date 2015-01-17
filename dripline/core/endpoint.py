56b56280e99e08384ea2ca0728aae79cc53b2ddd
Ben LaRoque <benjamin.laroque@gmail.com>
2015-01-16 17:37:21 -0800

    adding binding functions to the endpoint itself, first steps toward integrating those abstractions

--- dripline/core/endpoint.py
+++ dripline/core/endpoint.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 from abc import ABCMeta, abstractproperty, abstractmethod
-from .message import Message, RequestMessage
+from .message import Message, RequestMessage, ReplyMessage
 from . import constants
 import pika
 
@@ -21,6 +21,63 @@ class Endpoint(object):
     # @abstractmethod
     def on_config(self):
         raise NotImplementedError
+
+    def _send_reply(self, channel, properties, result):
+        '''
+        Send an AMQP reply
+        '''
+        reply = message.ReplyMessage(payload=result).to_msgpack()
+        channel.basic_publish(exchange='requests',
+                              routing_key=properties.reply_to,
+                              properties=pika.BasicProperties(
+                                correlation_id=properties.correlation_id
+                              ),
+                              body=reply,
+                             )
+
+    def handle_request(self, channel, method, properties, request):
+        '''
+        '''
+        msg = message.Message.from_msgpack(request)
+        logger.debug('got a {} request: {}'.format(msg.msgop, msg.payload))
+        #well this is still a hacky way to make a dict, but is better
+        #method_dict = {constants.OP_SENSOR_CONFIG:self.on_config,
+        #               constants.OP_SENSOR_GET:self.on_get,
+        #               constants.OP_SENSOR_SET:self.on_set,
+        #              }
+        # and this is somewhat gratuitus...
+        #m_dict = {getattr(c,k):'on_'+k.lstrip('OP_SENSOR_').lower() for k in dir(constants) if k.startswith('OP_SENSOR_')}
+        method_dict = {}
+        for key in dir(constants):
+            if k.startswith('OP_SENSOR_'):
+                method_dict[getattr(constants, key)] = 'on_'+key.lstrip('OP_SENSOR_').lower()
+                
+        result = None
+        try:
+            value = msg.payload
+            result = method_dict[msg.msgop](*value)
+            if result is None:
+                result = "operation returned None"
+        except Exception as err:
+            logger.error('got an error: {}'.format(err.message))
+            result = err.message
+        self._send_reply(channel, properties, result)
+        ########
+#    elif msg.msgop == constants.OP_SENSOR_CONFIG:
+#        logger.debug('got a {} request: {}'.format(msg.msgop, msg.payload))
+#        result = None
+#        try:
+#            value = msg.payload
+#            result = self.on_config(*value)
+#        except Exception as err:
+#            logger.warning("config failed: {}".format(err.message))
+#            result = err.message
+#        self._send_reply(channel, properties, result)
+#    else:
+#        logger.warning('Got an operation request of unsupported type')
+#        self._send_reply(channel, properties, "operation unknown")
+#        channel.basic_ack(delivery_tag=method.delivery_tag)
+
         
 class AutoReply(Endpoint):
     __metaclass__ = ABCMeta
