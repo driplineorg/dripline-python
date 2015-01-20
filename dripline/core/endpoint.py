7579c8d5d7d8a0d444d4599e949544db76f63782
Ben LaRoque <benjamin.laroque@gmail.com>
2015-01-19 17:44:34 -0800

    endpoint has request handling responsibility

--- dripline/core/endpoint.py
+++ dripline/core/endpoint.py
@@ -3,9 +3,15 @@ from __future__ import absolute_import
 from abc import ABCMeta, abstractproperty, abstractmethod
 from .message import Message, RequestMessage, ReplyMessage
 from . import constants
+
+import math
+import traceback
 import pika
 
 __all__ = ['Endpoint', 'AutoReply']
+
+import logging
+logger = logging.getLogger(__name__)
 
 class Endpoint(object):
     __metaclass__ = ABCMeta
@@ -14,6 +20,14 @@ class Endpoint(object):
         self.name = name
         self.provider = None
         self._calibration_str = cal_str
+
+        method_dict = {}
+        for key in dir(constants):
+            if key.startswith('OP_SENSOR_'):
+                method_name = 'on_'+key.replace('OP_SENSOR_','').lower()
+                method = getattr(self, method_name)
+                method_dict[getattr(constants, key)] = method
+        self.methods = method_dict
 
     # @abstractmethod
     def on_get(self):
@@ -24,90 +38,74 @@ class Endpoint(object):
         raise NotImplementedError
 
     # @abstractmethod
-    def on_config(self):
+    def on_config(self, attribute, value):
         raise NotImplementedError
 
-    def _send_reply(self, channel, properties, result):
+    def _send_reply(self, channel, properties, reply):
         '''
         Send an AMQP reply
         '''
-        reply = message.ReplyMessage(payload=result).to_msgpack()
+        if not isinstance(reply, ReplyMessage):
+            logger.warn('should be providing a ReplyMessage')
+            reply = ReplyMessage(payload=reply)
+        logger.debug('reply is: {}'.format(reply))
         channel.basic_publish(exchange='requests',
                               routing_key=properties.reply_to,
                               properties=pika.BasicProperties(
                                 correlation_id=properties.correlation_id
                               ),
-                              body=reply,
+                              body=reply.to_msgpack(),
                              )
 
     def _calibrate(self, raw):
         '''
         '''
         val_dict = {'value_raw':raw}
-        if not self._calib_str is None:
+        if not self._calibration_str is None:
             logger.info('adding calibrated value')
             globals = {"__builtins__": None,
                        "math": math,
                       }
             locals = {}
-            val_dict['value_cal'] = eval(self._calib_str.format(raw), globals, locals)
+            val_dict['value_cal'] = eval(self._calibration_str.format(raw), globals, locals)
         return val_dict
 
     def handle_request(self, channel, method, properties, request):
         '''
         '''
-        msg = message.Message.from_msgpack(request)
+        msg = Message.from_msgpack(request)
         logger.debug('got a {} request: {}'.format(msg.msgop, msg.payload))
 
-        # and this is somewhat gratuitus...
-        #m_dict = {getattr(c,k):'on_'+k.lstrip('OP_SENSOR_').lower() for k in dir(constants) if k.startswith('OP_SENSOR_')}
-        method_dict = {}
-        for key in dir(constants):
-            if k.startswith('OP_SENSOR_'):
-                method_dict[getattr(constants, key)] = 'on_'+key.lstrip('OP_SENSOR_').lower()
-                
         result = None
-        logger.debug('going to call: {}'.format(method_dict[msg.msgop]))
         try:
-            logger.debug('about to try')
             value = msg.payload
-            result = method_dict[msg.msgop](*value)
-            logger.debug('result received')
+            result = self.methods[msg.msgop](*value)
             if not self._calibration_str is None:
                 result = self._calibrate(result)
             if result is None:
                 result = "operation returned None"
         except Exception as err:
             logger.error('got an error: {}'.format(err.message))
+            logger.debug('traceback follows:\n{}'.format(traceback.format_exc()))
             result = err.message
-        self._send_reply(channel, properties, result)
-        ########
-#    elif msg.msgop == constants.OP_SENSOR_CONFIG:
-#        logger.debug('got a {} request: {}'.format(msg.msgop, msg.payload))
-#        result = None
-#        try:
-#            value = msg.payload
-#            result = self.on_config(*value)
-#        except Exception as err:
-#            logger.warning("config failed: {}".format(err.message))
-#            result = err.message
-#        self._send_reply(channel, properties, result)
-#    else:
-#        logger.warning('Got an operation request of unsupported type')
-#        self._send_reply(channel, properties, "operation unknown")
-#        channel.basic_ack(delivery_tag=method.delivery_tag)
+        logger.debug('result is: {}'.format(result))
+        reply = ReplyMessage(payload=result)
+        logger.debug('reply_message is: {}'.format(reply))
+        self._send_reply(channel, properties, reply)
+        logger.debug('reply sent')
 
         
 class AutoReply(Endpoint):
     __metaclass__ = ABCMeta
 
     def send_reply(self, channel, properties, result):
+        logger.debug('trying to send result: {}'.format(result))
         channel.basic_publish(exchange='requests',
                               routing_key=properties.reply_to,
                               properties=pika.BasicProperties(
                                   correlation_id=properties.correlation_id
                                   ),
-                              body=result)
+                              body=result.to_msgpack())
 
     def handle_request(self, channel, method, properties, request):
         msg = Message.from_msgpack(request)
