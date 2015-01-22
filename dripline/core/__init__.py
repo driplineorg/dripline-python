e7d053e133988c292c3a2c8c6c24d2bab873d899
Ben LaRoque <benjamin.laroque@gmail.com>
2015-01-21 23:33:56 -0800

    purging binding (now in endpoint) and node (mostly in portal)

--- dripline/core/__init__.py
+++ dripline/core/__init__.py
@@ -8,14 +8,11 @@ from __future__ import absolute_import
 
 from .alert_consumer import *
 from .arg_parser import *
-from .binding import *
-#from .config import *
 from .connection import *
 from .constants import *
 from .data_logger import *
 from .endpoint import *
 from .message import *
-from .node import *
 from .portal import *
 from .provider import *
 from .sensor import *
