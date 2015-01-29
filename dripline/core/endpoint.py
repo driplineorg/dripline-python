9c3361440ee2d9880c4e307caee4040aa8106cc9
Ben LaRoque <benjamin.laroque@gmail.com>
2015-01-29 00:18:48 -0800

    adding a manual calibration for pt100 (mostly for < -50 C)

--- dripline/core/endpoint.py
+++ dripline/core/endpoint.py
@@ -49,6 +49,20 @@ def cernox_calibration(resistance, serial_number):
     return math.exp(math.log(resistance)*m+b)
 
 
+def pt100_calibration(resistance):
+    r = resistance
+    value = ((2.2913 <= r and r < 3.65960) *((3.65960-r)*(-6.95647+r/0.085)/1.36831 + (r-2.2913)*(10.83979+r/.191)/1.36831 ) +
+        (3.6596 <= r and r < 9.38650) *((9.38650-r)*(10.83979+r/0.191)/5.72690 + (r-3.6596)*(23.92640+r/.360)/5.72690) +
+        (9.3865 <= r and r < 20.3800) *((20.3800-r)*(23.92640+r/0.360)/10.9935 + (r-9.3865)*(29.17033+r/.423)/10.9935) +
+        (20.380 <= r and r < 29.9290) *((29.9890-r)*(29.17033+r/0.423)/9.54900 + (r-20.380)*(29.10402+r/.423)/9.54900) +
+        (29.989 <= r and r < 50.7880) *((50.7880-r)*(29.10402+r/0.423)/20.7990 + (r-29.989)*(25.82396+r/.409)/20.7990) +
+        (50.788 <= r and r < 71.0110) *((71.0110-r)*(25.82396+r/0.409)/20.2230 + (r-50.788)*(22.47250+r/.400)/20.2230) +
+        (71.011 <= r and r < 90.8450) *((90.8450-r)*(22.47250+r/0.400)/19.8340 + (r-71.011)*(18.84224+r/.393)/19.8340) +
+        (90.845 <= r and r < 110.354) *((110.354-r)*(18.84224+r/0.393)/19.5090 + (r-90.845)*(14.84755+r/.387)/19.5090) +
+        (110.354 <= r) * (14.84755+r/.387))
+    return value
+
+
 def calibrate(fun):
     def wrapper(self):
         val_dict = {'value_raw':fun(self)}
@@ -57,6 +71,7 @@ def calibrate(fun):
             globals = {"__builtins__": None,
                        "math": math,
                        "cernox_calibration": cernox_calibration,
+                       "pt100_calibration": pt100_calibration,
                       }
             locals = {}
             cal = eval(self._calibration_str.format(val_dict['value_raw']), globals, locals)
