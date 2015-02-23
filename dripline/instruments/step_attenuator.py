2540731b69a3b8359294b643aaac954174a2805d
Megan Wachtendonk <mwachtendonk@darlene.(none)>
2015-02-23 22:51:01 +0000

    Modifications to step attenuator file

--- dripline/instruments/step_attenuator.py
+++ dripline/instruments/step_attenuator.py
@@ -2,12 +2,17 @@ from __future__ import absolute_import
 ##this makes sure python can use stuff from newer versions of python
 
 import logging
+import spidev
 ##here import outside libraries that you will use later
 
 from ..core import Endpoint
 ##if you needed to import anything from dripline (Spime, Provider, ect) import specific things here  
 
-logger= logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
+
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.xfer([10])
 
 __all__= ['StepAttenuator']
 ##this is what classes are exported when you try to use this file so that they go to the global namespace. Since StepAttenuator is the only class you've made this should be the only name in the list. 
@@ -15,7 +20,7 @@ __all__= ['StepAttenuator']
 class StepAttenuator(Endpoint):
     def __init__(self, file_name="/tmp/step_atten.txt", **kwargs):
         self.file_name=file_name
-        
+        Endpoint.__init__(self, **kwargs)         
 
     #**kwargs is a library that has arguments. If your class is given a name that it doesn't have a value/function for it will pass it to kwargs, which will pass it along to the parent class, which will do the same thing. 
     #If you haven't came up with something for your function to do, pass it so that it doesn't do anything but you can still run file.
@@ -25,8 +30,9 @@ class StepAttenuator(Endpoint):
         f= open(self.file_name, "r+")
         lines= f.readlines()
         last_line= lines[-1]
-        print last_line 
+        logger.debug("last value was: {}".format(last_line))
         f.close()
+        return last_line
 
 ##This opens up the temp file, which can be called/put where the user wants, and recalls the last line of the file
 
@@ -35,7 +41,7 @@ class StepAttenuator(Endpoint):
         self.value=value
         string=str(value)
         f.write(string)
-        print string
         f.close()
+        return string 
 
 ##This sets the new value for the SA but rewrites the file and deletes the old content. This is fine for now but it something to fix in the future. 
