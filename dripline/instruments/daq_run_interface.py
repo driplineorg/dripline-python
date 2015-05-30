cf39ecd1d71ca553204d977b3ca0c8c71374b633
Ben LaRoque <benjamin.laroque@gmail.com>
2015-05-30 15:02:26 -0700

    able to run mantis

--- dripline/instruments/daq_run_interface.py
+++ dripline/instruments/daq_run_interface.py
@@ -28,8 +28,8 @@ class DAQProvider(core.Provider):
         self.daq_name = daq_name
         self._run_name = None
         self._run_number = None
-        self._directory_path = directory_path
-        self._run_table_endpoint = run_table_endpoint
+        self.directory_path = directory_path
+        self.run_table_endpoint = run_table_endpoint
 
     @property
     def run_name(self):
@@ -58,6 +58,34 @@ class MantisProvider(DAQProvider, core.Spime):
         core.Spime.__init__(self, **kwargs)
         self.mantis_queue = mantis_queue
         self._acquisition_count = 0
+
+    def start_run(self):
+        pass
+
+    def on_get(self):
+        '''
+        Setting an on_get so that the logging functionality can be used to queue multiple acquisitions.
+        '''
+        if self._run_number is None:
+            raise core.DriplineInternalError('run number is None, must request a run_id assignment prior to starting acquisition')
+        _conn = core.Connection(self.portal.broker)
+        filepath = '{}/{:09d}_{:09d}.egg'.format(self.directory_path,
+                                         self._run_number,
+                                         self._acquisition_count)
+        request = core.RequestMessage(payload={'values':[], 'file':filepath},
+                                      msgop=core.OP_RUN,
+                                     )
+        result = _conn.send_request(self.mantis_queue,
+                                    request=request,
+                                    decode=True,
+                                   )
+        if not result.retcode == 0:
+            msg = ''
+            if 'ret_msg' in result.payload:
+                msg = result.payload['ret_msg']
+            logger.warning('got an error from mantis: {}'.format(msg))
+        else:
+            self._acquisition_count += 1
 
     def close_run(self):
         self._acquisition_count = 0
