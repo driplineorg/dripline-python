ce4c4f04671dc10c6e2db625b3897767f3bf181e
Ben LaRoque <benjamin.laroque@gmail.com>
2015-08-30 18:01:14 -0700

    calibration methods being moved out of core

--- dripline/instruments/kv_store.py
+++ dripline/instruments/kv_store.py
@@ -32,6 +32,12 @@ from ..core import Provider, Spime, calibrate
 __all__ = ['kv_store', 'kv_store_key']
 
 
+# Just a few dumb calibration functions for testing
+def times2(value):
+    return 2.*value
+def times3(value):
+    return 3.*value
+
 logger = logging.getLogger(__name__)
 class kv_store(Provider):
     """
@@ -72,7 +78,7 @@ class kv_store_key(Spime):
     def report_log(value):  
         logger.info("Should be logging value: {}\n".format(value))
 
-    @calibrate()
+    @calibrate([times2, times3])
     def on_get(self):
         """
         Return the value associated with this
