ae307c9ec35e0d5027f7a0cff84e27587477d91c
Ben LaRoque <benjamin.laroque@gmail.com>
2018-01-12 16:49:56 -0800

    bump again

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from glob import glob
 
-import sys
+import sys, os
 from setuptools.command.test import test as TestCommand
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
