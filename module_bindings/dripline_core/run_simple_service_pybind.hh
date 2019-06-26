d20067bd1cb67cbbb7ef2a0887516fde09f4662a
Eric Gonzalez <do_you_have_a_pnnl_email?>
2019-06-26 15:50:05 -0700

    Formatted some code (no functionality was changed)

--- module_bindings/dripline_core/run_simple_service_pybind.hh
+++ module_bindings/dripline_core/run_simple_service_pybind.hh
@@ -1,12 +1,14 @@
 #include "run_simple_service.hh"
 #include "pybind11/pybind11.h"
 
-namespace dripline_pybind {
-
-  void export_run_simple_service(pybind11::module& mod){
-    pybind11::class_<dripline::run_simple_service, std::shared_ptr<dripline::run_simple_service> >(mod, "run_simple_service")
-      .def(pybind11::init<const scarab::param_node&>())
-      .def("execute", &dripline::run_simple_service::execute);
+namespace dripline_pybind
+{
+  void export_run_simple_service( pybind11::module& mod )
+  {
+    pybind11::class_< dripline::run_simple_service, std::shared_ptr< dripline::run_simple_service > >
+      ( mod, "run_simple_service", "Minimal example of a dripline service" )
+      .def( pybind11::init< const scarab::param_node& >() )
+      .def( "execute", &dripline::run_simple_service::execute );
 
   } //end export_run_simple_service
 } //end dripline_pybind namespace
