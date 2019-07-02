09f76e68649c8e486f12b5420d49fb2b9fe5d707
Ben LaRoque <benjamin.laroque@gmail.com>
2019-07-02 13:45:21 -0700

    trying to normalize the code style

--- module_bindings/scarab_param/param_value_pybind.hh
+++ module_bindings/scarab_param/param_value_pybind.hh
@@ -15,6 +15,7 @@ namespace scarab_pybind
     {
         // param_value
         pybind11::class_< scarab::param_value, scarab::param >( mod, "ParamValue" )
+            // initialization overloads by type
             .def( pybind11::init< bool >() )
             .def( pybind11::init< unsigned >() )
             .def( pybind11::init< int >() )
@@ -22,25 +23,43 @@ namespace scarab_pybind
             .def( pybind11::init< const std::string& >() )
             .def( pybind11::init< const char* >() )
 
-            .def( "is_bool", (bool (scarab::param_value::*)() const) &scarab::param_value::is_bool, "Return whether the param_value stores a boolean value" )
-            .def( "is_uint", (bool (scarab::param_value::*)() const) &scarab::param_value::is_uint, "Return whether the param_value stores an unsigned integer value" )
-            .def( "is_int", (bool (scarab::param_value::*)() const) &scarab::param_value::is_int, "Return whether the param_value stores a signed integer value" )
-            .def( "is_double", (bool (scarab::param_value::*)() const) &scarab::param_value::is_double, "Return whether the param_value stores a double value" )
-            .def( "is_string", (bool (scarab::param_value::*)() const) &scarab::param_value::is_string, "Return whether the param_value stores a string value" )
+            // type checking methods
+            .def( "is_bool", (bool (scarab::param_value::*)() const) &scarab::param_value::is_bool,
+                    "Return whether the param_value stores a boolean value" )
+            .def( "is_uint", (bool (scarab::param_value::*)() const) &scarab::param_value::is_uint,
+                    "Return whether the param_value stores an unsigned integer value" )
+            .def( "is_int", (bool (scarab::param_value::*)() const) &scarab::param_value::is_int,
+                    "Return whether the param_value stores a signed integer value" )
+            .def( "is_double", (bool (scarab::param_value::*)() const) &scarab::param_value::is_double,
+                    "Return whether the param_value stores a double value" )
+            .def( "is_string", (bool (scarab::param_value::*)() const) &scarab::param_value::is_string,
+                    "Return whether the param_value stores a string value" )
 
-            .def( "as_bool", (bool (scarab::param_value::*)() const) &scarab::param_value::as_bool, "Get parameter value as a bool" )
-            .def( "as_uint", (unsigned (scarab::param_value::*)() const) &scarab::param_value::as_uint, "Get parameter value as an unsigned integer" )
-            .def( "as_int", (int (scarab::param_value::*)() const) &scarab::param_value::as_int, "Get parameter value as a signed integer" )
-            .def( "as_double", (double (scarab::param_value::*)() const) &scarab::param_value::as_double, "Get parameter value as a float" )
-            .def( "as_string", (const std::string& (scarab::param_value::*)() const) &scarab::param_value::as_string, "Get parameter value as a string" )
+            // output cast to native types
+            .def( "as_bool", (bool (scarab::param_value::*)() const) &scarab::param_value::as_bool,
+                    "Get parameter value as a bool" )
+            .def( "as_uint", (unsigned (scarab::param_value::*)() const) &scarab::param_value::as_uint,
+                    "Get parameter value as an unsigned integer" )
+            .def( "as_int", (int (scarab::param_value::*)() const) &scarab::param_value::as_int,
+                    "Get parameter value as a signed integer" )
+            .def( "as_double", (double (scarab::param_value::*)() const) &scarab::param_value::as_double,
+                    "Get parameter value as a float" )
+            .def( "as_string", (const std::string& (scarab::param_value::*)() const) &scarab::param_value::as_string,
+                    "Get parameter value as a string" )
 
-            .def( "set", (void (scarab::param_value::*)(bool)) &scarab::param_value::set, "Set a bool value" )
+            // set method overloads
+            .def( "set", (void (scarab::param_value::*)(bool)) &scarab::param_value::set,
+                    "Set a bool value" )
             //.def( "set", (void (scarab::param_value::*)(unsigned)) &scarab::param_value::set<uint64_t>, "Set an unsigned integer value" )
-            .def("set",&scarab::param_value::set<uint64_t>, "Set an unsigned integer value")
+            .def("set",&scarab::param_value::set<uint64_t>,
+                    "Set an unsigned integer value")
             //.def( "set", (void (scarab::param_value::*)(int)) &scarab::param_value::set<int64_t>, "Set a signed integer value" )
-            .def("set", &scarab::param_value::set<int64_t>, "Set a signed integer value")
-            .def( "set", (void (scarab::param_value::*)(double)) &scarab::param_value::set, "Set an float value" )
-            .def( "set", (void (scarab::param_value::*)(std::string)) &scarab::param_value::set, "Set an string value" )
+            .def("set", &scarab::param_value::set<int64_t>,
+                    "Set a signed integer value")
+            .def( "set", (void (scarab::param_value::*)(double)) &scarab::param_value::set,
+                    "Set an float value" )
+            .def( "set", (void (scarab::param_value::*)(std::string)) &scarab::param_value::set,
+                    "Set an string value" )
             ;
     }
 
--- module_bindings/scarab_param/scarab_param_namespace_pybind.cc
+++ module_bindings/scarab_param/scarab_param_namespace_pybind.cc
@@ -5,21 +5,20 @@
  *     Author: B.H. LaRoque
  */
 
+
 #include "param_pybind.hh"
 #include "param_value_pybind.hh"
 #include "param_array_pybind.hh"
 #include "param_node_pybind.hh"
-#include "signal_handler.hh"
+#include "signal_handler_pybind.hh"
 
 PYBIND11_MODULE( scarab, scarab_mod )
 {
-    pybind11::module param_mod = scarab_mod.def_submodule("param", "param data types")
-            .def("cancel_all", [](int a_code)
-                    {
-                        return scarab::signal_handler::cancel_all(a_code);
-                    });
-    scarab_pybind::export_param(param_mod);
-    scarab_pybind::export_param_value(param_mod);
-    scarab_pybind::export_param_array(param_mod);
-    scarab_pybind::export_param_node(param_mod);
+    // Param-related data objects
+    scarab_pybind::export_param( scarab_mod );
+    scarab_pybind::export_param_value( scarab_mod );
+    scarab_pybind::export_param_array( scarab_mod );
+    scarab_pybind::export_param_node( scarab_mod );
+    // signal handler
+    scarab_pybind::export_signal_handler( scarab_mod );
 }
