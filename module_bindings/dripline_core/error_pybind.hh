d87083a7e9a359bf76eda4ac0dc1330fed909c94
Ben LaRoque <benjamin.laroque@gmail.com>
2019-07-09 10:21:14 -0700

    error readability

--- module_bindings/dripline_core/error_pybind.hh
+++ module_bindings/dripline_core/error_pybind.hh
@@ -5,22 +5,23 @@
 namespace dripline_pybind
 {
 
-    void export_error(pybind11::module& mod)
+    void export_error( pybind11::module& mod )
     {
 
-        static pybind11::exception< dripline::dripline_error > ex(mod, "dripline_error");
-        pybind11::register_exception_translator([](std::exception_ptr p)
+        static pybind11::exception< dripline::dripline_error > ex( mod, "dripline_error" );
+        pybind11::register_exception_translator( [](std::exception_ptr p)
         {
             try
             {
-                if (p) std::rethrow_exception(p);
+                if ( p ) std::rethrow_exception( p );
             }
-            catch (const dripline::dripline_error &e)
+            catch ( const dripline::dripline_error &e )
             {
                 // Set dripline_error as the active python error
-                ex(e.what());
+                ex( e.what() );
             }
-        });
+        }
+        );
 
     }
 
