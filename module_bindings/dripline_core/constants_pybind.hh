78d42f849d314d0728feb61458f56e5be0922347
Ben LaRoque <benjamin.laroque@gmail.com>
2019-07-09 10:15:01 -0700

    more readability in constants

--- module_bindings/dripline_core/constants_pybind.hh
+++ module_bindings/dripline_core/constants_pybind.hh
@@ -8,19 +8,24 @@ namespace dripline_pybind
     void export_constants(pybind11::module& mod)
     {
 
-        pybind11::enum_< dripline::op_t >(mod, "op_t", pybind11::arithmetic())
+        pybind11::enum_< dripline::op_t >( mod, "op_t", pybind11::arithmetic() )
                 // the values
-                .value("set", dripline::op_t::set).value("get", dripline::op_t::get).value("config", dripline::op_t::config) // deprecated as of v2.0.0 (dripline_cpp)
-                .value("send", dripline::op_t::send).value("run", dripline::op_t::run).value("cmd", dripline::op_t::cmd).value("unknown", dripline::op_t::unknown)
+                .value( "set", dripline::op_t::set )
+                .value( "get", dripline::op_t::get )
+                .value( "config", dripline::op_t::config ) // deprecated as of v2.0.0 (dripline_cpp)
+                .value( "send", dripline::op_t::send )
+                .value( "run", dripline::op_t::run )
+                .value( "cmd", dripline::op_t::cmd )
+                .value( "unknown", dripline::op_t::unknown )
                 // helpers for type conversion
-                .def("to_uint", (uint32_t (*)(dripline::op_t))&dripline::to_uint, "Convert an op_t to int")
+                .def( "to_uint", (uint32_t (*)(dripline::op_t))&dripline::to_uint, "Convert an op_t to int" )
                 .def( "to_string", (std::string (*)(dripline::op_t))&dripline::to_string, "Convert an op_t to string" )
                 .def_static( "to_op_t", (dripline::op_t (*)(uint32_t))&dripline::to_op_t, "Convert an uint to op_t" )
                 .def_static( "to_op_t", (dripline::op_t (*)(std::string))&dripline::to_op_t, "Convert an string to op_t" )
                 ;
 
         pybind11::enum_<dripline::msg_t>( mod, "msg_t", pybind11::arithmetic() )
-        // the values
+                // the values
                 .value( "reply", dripline::msg_t::reply )
                 .value( "request", dripline::msg_t::request )
                 .value( "alert", dripline::msg_t::alert )
