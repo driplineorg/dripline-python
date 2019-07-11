65d0f28b5513cad8ed11510076e4f509856f1311
Eric Gonzalez <do_you_have_a_pnnl_email?>
2019-07-11 11:30:44 -0700

    Continuing work on Endpoint

--- dripline/endpoint.py
+++ dripline/endpoint.py
@@ -1,9 +1,11 @@
-class Endpoint(_Endpoint):
+import dripline
+class Endpoint(dripline.core._Endpoint):
     def __init__( self, _name ):
         self.name = _name
         
-    def do_get( self ):
-        return self.value
+    def do_get( self, _message ):
+        if ( _message.is_request() ):
+            return dripline.core._Endpoint.do_get_request( _message )
         
     def do_set( self, _value ):
         self.value = _value
