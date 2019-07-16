2ba37580b512f053612fee4ee9374d2147a961bf
Eric Gonzalez <do_you_have_a_pnnl_email?>
2019-07-15 17:34:59 -0700

    Continued work on serve

--- dripline/serve.py
+++ dripline/serve.py
@@ -1,8 +1,13 @@
 import yaml
-  
-file_name = file( "../examples/kv_store_tutorial.yaml", "r" )
+from collections import OrderedDict
+
+with open( "../examples/kv_store_tutorial.yaml", "r" ) as stream:
+    try:
+        store = yaml.safe_load( stream )
+        print ( yaml.dump(store) )
+    except yaml.YAMLError as exc:
+        print( exc )
+        
+#file_name = file( "../examples/kv_store_tutorial.yaml", "r" )
 #print yaml.dump( yaml.load(file_name), default_flow_style=False )
 
-for data in yaml.load(file_name):
-    print data
-    print "-----------------------------------------"
