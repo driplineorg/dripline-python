d4f4a984fa4a7550a9f1c710276d2853b06a9369
Ben LaRoque <benjamin.laroque@gmail.com>
2019-07-18 15:19:12 -0700

    fixing default arguments in constructor for Service

--- module_bindings/dripline_core/service_pybind.hh
+++ module_bindings/dripline_core/service_pybind.hh
@@ -11,17 +11,30 @@ namespace dripline_pybind
     void export_service( pybind11::module& mod )
     {
         pybind11::class_< dripline::service, std::shared_ptr< dripline::service > >( mod, "Service", "Used to send and receive simple messages" )
-            
-             .def( pybind11::init< pybind11::arg("a_config") = const scarab_pybind::ParamNode(), pybind11::arg("a_queue_name") = const "", pybind11::arg("a_broker_address") = const "", pybind11::arg("a_port") = const 0, pybind11::arg("a_auth_file") = const "", pybind11::arg("a_make_connection") = const 1 >() )
-            
+            .def( pybind11::init< const scarab::param_node&,
+                                   const std::string&,
+                                   const std::string&,
+                                   const unsigned int,
+                                   const std::string&,
+                                   const bool
+                                 >(),
+                   pybind11::arg( "a_config" ) = scarab::param_node(),
+                   pybind11::arg( "a_queue_name" ) = "",
+                   pybind11::arg( "a_broker_address" ) = "",
+                   pybind11::arg( "a_port" ) = 0,
+                   pybind11::arg( "a_auth_file" ) = "",
+                   pybind11::arg( "a_make_connection" ) = true
+            )
+
             /* ORIGINAL INIT<> FUNCTION */
             /*  .def( pybind11::init< const scarab::param_node&, const std::string&, const std::string&, const unsigned int, const std::string&, const bool >() ) */
-                .def( "start", &dripline::service::start )
-                .def( "listen", &dripline::service::listen )
-                .def( "stop", &dripline::service::stop )
-                .def( "add_child", &dripline::service::add_child )
-                .def( "add_async_child", &dripline::service::add_async_child )
-                .def( "noisy_func", []() { pybind11::scoped_ostream_redirect stream(std::cout, pybind11::module::import("sys").attr("stdout"));});
+            .def( "start", &dripline::service::start )
+            .def( "listen", &dripline::service::listen )
+            .def( "stop", &dripline::service::stop )
+            .def( "add_child", &dripline::service::add_child )
+            .def( "add_async_child", &dripline::service::add_async_child )
+            .def( "noisy_func", []() { pybind11::scoped_ostream_redirect stream(std::cout, pybind11::module::import("sys").attr("stdout"));})
+            ;
     }
 } /* namespace dripline_pybind */
 
