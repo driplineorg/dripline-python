315aaa0ec4a7f9e28590d87d46080c119b51a1c5
Eric Gonzalez <do_you_have_a_pnnl_email?>
2019-08-09 12:15:48 -0700

    do_set_request now functions with int and double values

--- dripline/endpoint.py
+++ dripline/endpoint.py
@@ -21,35 +21,35 @@ class Endpoint(dripline.core._Endpoint):
                     the_node.add( "values", the_array )
                     return a_request_message.reply( 0, "Success", the_node )
                 else:
-                    return a_request_message.reply( 200, "Dripline.NotImplemented temporary message" )
+                    return a_request_message.reply( 200,  200, "Attribute {} does not exist in endpoint <{}>".format(a_specifier, self.name) )
             except AttributeError:
-                return a_request_message.reply( 201, "Attribute '" + a_specifier + "' does not exist in endpoint <" + self.name + ">" )
+                return a_request_message.reply( 201, "attribute error: {}".format(this_error.message) )
         else:
             return self.on_get( a_request_message )
 
     def on_get( self, a_request ):
-        return a_request.reply( 202, "Endpoint <{}> of type {} does not support Get".format(self.name, type(self)) )
+        return a_request.reply( 203, "Endpoint <{}> of type {} does not support Get".format(self.name, type(self)) )
 
     def do_set_request( self, a_request_message ):
         a_specifier =  a_request_message.parsed_specifier().to_string()
         if ( a_specifier ):
             try:
                 if ( getattr(self, a_specifier, "NotFound") != "NotFound"):
-                        if( isinstance(getattr(self, a_specifier, "NotFound"), int) ):
-                            a_payload_value = a_request_message.payload()["values"][0]().as_int()
-                        elif( isinstance(getattr(self, a_specifier, "NotFound"), float) ):
-                            a_payload_value = a_request_message.payload()["values"][0]().as_double()
+                    print( "Current value of {} is: {}".format(a_specifier, getattr(self, a_specifier, "NotFound")) )
+                    if( isinstance(getattr(self, a_specifier, "NotFound"), int) ):
+                        a_payload_value = a_request_message.payload()["values"][0]().as_int()
+                        
+                    elif( isinstance(getattr(self, a_specifier, "NotFound"), float) ):
+                        a_payload_value = a_request_message.payload()["values"][0]().as_double()
+                        
                     print( "Request message payload: {}".format(a_request_message.payload()) )
                     print( "Payload value: {}".format(a_payload_value) )
-                    #self.test_attribute = a_request_message.payload().at("values")
                     setattr( self, a_specifier, a_payload_value)
-                    print( "Changed {} to: {}".format(a_specifier, getattr(self, a_specifier, "NotFound")) ) 
-                    return a_request_message.reply( 201, "TESTING" )
+                    return a_request_message.reply( 0, "Changed {} to: {}".format(a_specifier, getattr(self, a_specifier, "NotFound")) )
                 else:
-                    return a_request_message.reply( 200, "Dripline.NotImplemented temporary message" )
+                    return a_request_message.reply( 200, "Attribute {} does not exist in endpoint <{}>".format(a_specifier, self.name) )
             except AttributeError as this_error:
-                print( "attribute error: {}".format(this_error.message) )
-                return a_request_message.reply( 201, "Attribute '" + a_specifier + "' does not exist in endpoint <" + self.name + ">" )
+                return a_request_message.reply( 201, "attribute error: {}".format(this_error.message) )
         else:
             return self.on_get( a_request_message )
 
