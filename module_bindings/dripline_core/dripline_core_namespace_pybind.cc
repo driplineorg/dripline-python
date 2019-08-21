3ed391ed9e55aeb5cecba925c8b7d027829433be
Ben LaRoque <benjamin.laroque@gmail.com>
2019-08-20 19:05:22 -0700

    more of all the things

--- module_bindings/dripline_core/dripline_core_namespace_pybind.cc
+++ module_bindings/dripline_core/dripline_core_namespace_pybind.cc
@@ -16,8 +16,8 @@ PYBIND11_MODULE( _dripline, dripline_mod )
     pybind11::module dripline_core_mod = dripline_mod.def_submodule( "core", "Core dripline standard implementation classes" );
     // Call exporters for the dripline.core namespace
     all_members.splice( all_members.end(), dripline_pybind::export_constants( dripline_core_mod ) );
-    dripline_pybind::export_endpoint( dripline_core_mod );
-    dripline_pybind::export_error( dripline_core_mod );
+    all_members.splice( all_members.end(), dripline_pybind::export_endpoint( dripline_core_mod ) );
+    all_members.splice( all_members.end(), dripline_pybind::export_error( dripline_core_mod ) );
     dripline_pybind::export_message( dripline_core_mod );
     dripline_pybind::export_run_simple_service( dripline_core_mod );
     dripline_pybind::export_specifier( dripline_core_mod );
