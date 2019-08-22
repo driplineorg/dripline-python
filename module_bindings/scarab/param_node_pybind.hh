f3ed00422a7d7d6c1e23c55af6d64eb13a130ef0
Ben LaRoque <benjamin.laroque@gmail.com>
2019-08-22 16:57:21 -0700

    more param_node binding

--- module_bindings/scarab/param_node_pybind.hh
+++ module_bindings/scarab/param_node_pybind.hh
@@ -15,11 +15,15 @@ namespace scarab_pybind
         // param_node
         pybind11::class_< scarab::param_node, scarab::param >( mod, "ParamNode" )
             .def( pybind11::init< >() )
-            .def( "__str__", &scarab::param_node::to_string )
 
+            .def( "__str__", &scarab::param_node::to_string )
+            .def( "__len__", &scarab::param_node::size )
             .def( "__getitem__", (scarab::param& (scarab::param_node::*)(const std::string&)) &scarab::param_node::operator[],
                     pybind11::return_value_policy::reference_internal)
-            .def( "__len__", &scarab::param_node::size )
+            .def( "__setitem__", [](scarab::param_node& an_obj, std::string& a_name, scarab::param& a_value){ an_obj.replace( a_name, a_value ); } )
+            .def( "__contains__", &scarab::param_node::has )
+
+            .def( "count", &scarab::param_node::count )
 
             .def( "is_null", &scarab::param_node::is_null )
             .def( "is_node", &scarab::param_node::is_node )
@@ -27,11 +31,6 @@ namespace scarab_pybind
             //TODO: has_subset()
 
             .def( "empty", &scarab::param_node::empty )
-
-            //TODO: are there more pythonic ways to do these?
-            .def( "has", &scarab::param_node::has )
-            .def( "count", &scarab::param_node::count )
-
 
             .def( "add", (bool (scarab::param_node::*)(const std::string&, const scarab::param&)) &scarab::param_node::add,
                     "Add a param object to a node")
