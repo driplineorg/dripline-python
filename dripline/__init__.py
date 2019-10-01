c9c2019d6fb5babd360ccdfea49188fb6072ba42
Ben LaRoque <benjamin.laroque@gmail.com>
2019-10-01 15:28:44 -0700

    adding implementations namespace

--- dripline/__init__.py
+++ dripline/__init__.py
@@ -1,4 +1,5 @@
 from . import core
+from . import implementations
 from . import extensions
 
 def __update_plugins():
@@ -9,5 +10,6 @@ def __update_plugins():
         name: importlib.import_module(name) for finder, name, ispkg in pkgutil.iter_modules(dripline.extensions.__path__, dripline.extensions.__name__+'.')
     }
     for finder, name, ispkg in pkgutil.iter_modules(dripline.extensions.__path__, dripline.extensions.__name__+'.'):
+        #print("adding: '{}'".format(name))
         setattr( dripline.extensions, name.split('dripline.extensions.')[1], importlib.import_module(name))
 __update_plugins()
