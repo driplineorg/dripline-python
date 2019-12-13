5927bb7b8dca5948441540a527c7bfef3c6eecbd
Ben LaRoque <benjamin.laroque@gmail.com>
2019-12-13 11:18:09 -0800

    adding simple key-value store implementation

---
+++ dripline/implementations/__init__.py
@@ -0,0 +1,1 @@
+from .key_value_store import *
---
+++ dripline/implementations/key_value_store.py
@@ -0,0 +1,19 @@
+from dripline.core import calibrate
+from dripline.core import Entity
+__all__ = []
+
+__all__.append("KeyValueStore")
+class KeyValueStore(Entity):
+
+    def __init__(self, initial_value=None, **kwargs):
+        Entity.__init__(self, **kwargs)
+        self._value = initial_value
+
+    @calibrate()
+    def on_get(self):
+        print("in K.V.S. on_get")
+        return self._value
+
+    def on_set(self, new_value):
+        print("in K.V.S. on_set")
+        self._value = new_value
