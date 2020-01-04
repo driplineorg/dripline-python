c93dddbc3d9ee0b69a80144d5396bedb3d6f9a6a
Ben LaRoque <benjamin.laroque@gmail.com>
2020-01-03 23:24:16 -0800

    stubbing out a bit more

--- dripline/core/alert_consumer.py
+++ dripline/core/alert_consumer.py
@@ -4,6 +4,14 @@ __all__ = []
 
 __all__.append('AlertConsumer')
 class AlertConsumer(Service):
+    '''
+    A base class for implementing custom alert message consumers.
+
+    One is expected to extend this class in one of two ways:
+    1) More advanced: override the existing on_alert_message method with whatever behavior is desired
+    2) Use the existing on_alert_message, which proceeds in two steps by calling parse_routing_key, followed by process_payload.
+       The first may be used or overriden, the second must always be implemented.
+    '''
     def __init__(self, alert_keys=["#"], **kwargs):
         print("in AlertConsumer init")
         Service.__init__(self, **kwargs)
@@ -19,3 +27,11 @@ class AlertConsumer(Service):
 
     def on_alert_message(self, an_alert):
         print("in python's on alert")
+        routing_data = self.parse_routing_key(an_alert.routing_key)
+        self.process_payload(an_alert.payload, routing_data)
+
+    def parse_routing_key(self, a_routing_key):
+        pass
+
+    def process_payload(self, a_payload, a_routing_key_data):
+        pass
