ecddec5e3b4b9da3a91997776501b6bd7f452ff4
Ben LaRoque <benjamin.laroque@gmail.com>
2020-01-07 15:23:52 -0800

    trying build again

--- .travis/setup_emulation.sh
+++ .travis/setup_emulation.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-
 echo '{"experimental": true}' | sudo tee -a /etc/docker/daemon.json
+echo "docker daemon config should be: `cat /etc/dockerr/daemon.json`"
+
 sudo service docker restart
+sudo systemctl restart docker
 
 echo 'checking for docker experimental...'
 docker version -f '{{.Server.Experimental}}'
