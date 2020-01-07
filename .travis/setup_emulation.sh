82aa7561ef0587553c4fc919df3ffbc87c4a41bf
Ben LaRoque <benjamin.laroque@gmail.com>
2020-01-07 14:55:11 -0800

    shouldn't be it

--- .travis/setup_emulation.sh
+++ .travis/setup_emulation.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-echo '{"experimental":true}' | sudo tee -a /etc/docker/daemon.json
+echo '{"experimental": true}' | sudo tee -a /etc/docker/daemon.json
 sudo service docker restart
 
 echo 'checking for docker experimental...'
