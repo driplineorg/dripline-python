9e44f68921920cf6bda38e79504b415b6864fdf6
Noah Oblath <nsoblath@gmail.com>
2020-01-07 11:40:15 -0800

    Commented out debugging code, and cleaned things up

--- dripline/core/throw_reply.py
+++ dripline/core/throw_reply.py
@@ -1,14 +1,14 @@
 __all__ = []
 
 import scarab
-from _dripline.core import _ThrowReply, DL_Success, set_reply_cache, throw_reply_keyword
+from _dripline.core import _ThrowReply, DL_Success, set_reply_cache
 
-from _dripline.core import _Base
+#from _dripline.core import _Base
 
 __all__.append('ThrowReply')
 class ThrowReply(Exception):
     def __init__(self, return_code=DL_Success(), message=DL_Success().description, payload=scarab.Param()):
-        Exception.__init__(self, throw_reply_keyword)
+        Exception.__init__(self)
         set_reply_cache(return_code, message, payload)
         #self.internal = _ThrowReply(return_code, payload)
         #self.internal.return_message(message)
@@ -16,20 +16,20 @@ class ThrowReply(Exception):
 
 
 
-__all__.append('Derived')
-class Derived(_Base):
-    def label(self):
-        return "Derived"
+#__all__.append('Derived')
+#class Derived(_Base):
+#    def label(self):
+#        return "Derived"
+#
+#    def throw_exception(self):
+#        raise Exception('Thrown from derived')
 
-    def throw_exception(self):
-        raise Exception('Thrown from derived')
-
-__all__.append('Derived2')
-class Derived2(_Base):
-    def label(self):
-        return "Derived2"
-
-    def throw_exception(self):
-        raise ThrowReply()
+#__all__.append('Derived2')
+#class Derived2(_Base):
+#    def label(self):
+#        return "Derived2"
+#
+#    def throw_exception(self):
+#        raise ThrowReply()
 
 
--- module_bindings/dripline_core/throw_reply_pybind.hh
+++ module_bindings/dripline_core/throw_reply_pybind.hh
@@ -3,12 +3,14 @@
 
 #include "reply_cache.hh"
 #include "dripline_fwd.hh"
+#include "endpoint.hh"
 
 #include "pybind11/pybind11.h"
 #include "pybind11/iostream.h"
 
 namespace dripline_pybind
 {
+    /*
     struct base
     {
         base() {}
@@ -35,6 +37,37 @@ namespace dripline_pybind
             catch( const pybind11::error_already_set& e )
             {
                 std::cerr << "pybind11::error_already_set:\n" << e.what() << "-----" << std::endl;
+
+                std::cerr << "Test whether the keyword matching works:" << std::endl;
+                if( std::string(e.what()).substr(0, dripline::throw_reply::py_throw_reply_keyword().size()) == dripline::throw_reply::py_throw_reply_keyword() )
+                {
+                    std::cerr << "Yes, this has been determined to be a throw_reply" << std::endl;
+
+                    reply_cache* t_reply_cache = reply_cache::get_instance();
+                    if( t_reply_cache->ret_code().rc_value() == dl_success::s_value )
+                    {
+                        LINFO( dlog, "Replying with: " << t_reply_cache->return_message() );
+                    }
+                    else
+                    {
+                        LWARN( dlog, "Replying with: " << t_reply_cache->return_message() );
+                    }
+                    t_reply = a_request->reply( t_reply_cache->ret_code(),t_reply_cache->return_message() );
+                    t_reply->set_payload( t_reply_cache->get_payload_ptr()->clone() );
+                    // don't rethrow a throw_reply
+                    // reply to be sent outside the catch block
+                }
+                else
+                {
+                    std::cerr << "No, this has been determined to be not-a-throw_reply" << std::endl;
+
+                    // treat the python exception as a standard exception
+                    LERROR( dlog, "Caught exception from Python: " << e.what() );
+                    t_reply = a_request->reply( dl_unhandled_exception(), e.what() );
+                    t_replier(); // send the reply before rethrowing
+                    throw; // unhandled exceptions should rethrow because they're by definition unhandled
+                }
+
             }
             catch( const std::runtime_error& e )
             {
@@ -75,11 +108,11 @@ namespace dripline_pybind
         the_base.execute();
         return;
     }
-
+*/
     std::list< std::string>  export_throw_reply( pybind11::module& mod )
     {
         std::list< std::string > all_items;
-
+/*
         all_items.push_back( "_Base" );
         pybind11::class_< base, base_trampoline >( mod, "_Base", "Base test class" )
             .def( pybind11::init<>() )
@@ -90,7 +123,7 @@ namespace dripline_pybind
 
         all_items.push_back( "test_throw" );
         mod.def( "test_throw", &test_throw );
-
+*/
 
         all_items.push_back( "_ThrowReply" );
         pybind11::class_< dripline::throw_reply >( mod, "_ThrowReply", "Holds information for creating a reply as a thrown exception")
@@ -125,14 +158,14 @@ namespace dripline_pybind
                            pybind11::return_value_policy::reference_internal
                          )
             ;
-
+/*
         all_items.push_back( "set_reply_cache1" );
         mod.def( "set_reply_cache1",
                  (void (*)(const dripline::throw_reply&)) &dripline::set_reply_cache1,
                  pybind11::arg( "throw_reply" ),
                  "set the reply_cache with the desired throw_reply"
              );
-
+*/
         all_items.push_back( "set_reply_cache" );
         mod.def( "set_reply_cache",
                  [](const dripline::return_code& a_code, const std::string& a_message, const scarab::param& a_payload) {
@@ -144,8 +177,8 @@ namespace dripline_pybind
                  "set the reply cache with the desired information" 
             );
 
-        all_items.push_back( "throw_reply_keyword" );
-        mod.attr( "throw_reply_keyword" ) = STRINGIFY(PYTHON_THROW_REPLY_KEYWORD);
+        //all_items.push_back( "throw_reply_keyword" );
+        //mod.attr( "throw_reply_keyword" ) = TOSTRING(PYTHON_THROW_REPLY_KEYWORD);
 
         return all_items;
     }
