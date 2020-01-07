6135caa21aff1918aa4783a17da4b1c2240ca3ca
Ben LaRoque <benjamin.laroque@gmail.com>
2020-01-07 14:45:40 -0800

    debugging echo

--- .travis/setup_emulation.sh
+++ .travis/setup_emulation.sh
@@ -4,4 +4,7 @@
 echo '{"experimental":true}' | sudo tee -a /etc/docker/daemon.json
 sudo service docker restart
 
+echo 'checking for docker experimental...'
+docker version -f '{{.Server.Experimental}}'
+
 sudo docker run --rm --privileged multiarch/qemu-user-static:register --reset
