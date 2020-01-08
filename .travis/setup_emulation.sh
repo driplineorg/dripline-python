f3db48b87dd8ced68a56e8157a0e6bf7afc44296
Ben LaRoque <benjamin.laroque@gmail.com>
2020-01-07 16:13:26 -0800

    file perms maybe

--- .travis/setup_emulation.sh
+++ .travis/setup_emulation.sh
@@ -4,14 +4,16 @@ docker_daemon_conf_file=/etc/docker/daemon.json
 
 set -ex
 echo "docker status:"
-systemctl status docker
+#systemctl status docker
 echo "initial docker daemon config should be: `sudo cat $docker_daemon_conf_file || true`"
 sudo ls -l $docker_daemon_conf_file
 
 #echo '{"experimental": true}' | sudo tee -a /etc/docker/daemon.json > /dev/null
 sudo cat $docker_daemon_conf_file | jq '. + {"experimental": true}' | sudo tee $docker_daemon_conf_file > /dev/null
 echo "docker daemon config should be: `sudo cat $docker_daemon_conf_file || true`"
+sudo ls -l $docker_daemon_conf_file
 
+echo 'restarting docker'
 sudo service docker restart
 sudo systemctl restart docker
 systemctl status docker
