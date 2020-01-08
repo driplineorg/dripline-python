9f6c4d04db1bd632685d1f7a77dc3949a2c17cd3
Ben LaRoque <benjamin.laroque@gmail.com>
2020-01-07 17:06:48 -0800

    seems to actually be working now on bionic

--- .travis/setup_emulation.sh
+++ .travis/setup_emulation.sh
@@ -3,28 +3,24 @@
 docker_daemon_conf_file=/etc/docker/daemon.json
 
 set -ex
+
+## initial check of docker config
 echo "docker status:"
-#systemctl status docker
 echo "initial docker daemon config should be: `sudo cat $docker_daemon_conf_file || true`"
-sudo ls -l $docker_daemon_conf_file
 docker version -f '{{.Server.Experimental}}'
 
-#echo '{"experimental": true}' | sudo tee -a /etc/docker/daemon.json > /dev/null
+## update docker config
 sudo cat $docker_daemon_conf_file | jq '. + {"experimental": true}' | sudo tee $docker_daemon_conf_file > /dev/null
 echo "docker daemon config should be: `sudo cat $docker_daemon_conf_file || true`"
-sudo ls -l $docker_daemon_conf_file
 
+## apply new docker config and check
 echo 'restarting docker'
 sudo service docker restart
 systemctl status docker
 docker version -f '{{.Server.Experimental}}'
-echo "docker daemon config should be: `sudo cat $docker_daemon_conf_file || true`"
+echo "docker daemon config should now be: `sudo cat $docker_daemon_conf_file || true`"
 
-#sudo systemctl restart docker
-#systemctl status docker
-
-echo 'checking for docker experimental...'
-docker version -f '{{.Server.Experimental}}'
-
+## register qemu in kernel
 sudo docker run --rm --privileged multiarch/qemu-user-static:register --reset
+
 set +ex
