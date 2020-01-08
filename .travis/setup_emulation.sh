c9be0bfdace8c6b25f0103869d1c05a807cbbf72
Ben LaRoque <benjamin.laroque@gmail.com>
2020-01-07 16:25:26 -0800

    ummm

--- .travis/setup_emulation.sh
+++ .travis/setup_emulation.sh
@@ -7,6 +7,7 @@ echo "docker status:"
 #systemctl status docker
 echo "initial docker daemon config should be: `sudo cat $docker_daemon_conf_file || true`"
 sudo ls -l $docker_daemon_conf_file
+docker version -f '{{.Server.Experimental}}'
 
 #echo '{"experimental": true}' | sudo tee -a /etc/docker/daemon.json > /dev/null
 sudo cat $docker_daemon_conf_file | jq '. + {"experimental": true}' | sudo tee $docker_daemon_conf_file > /dev/null
@@ -16,8 +17,11 @@ sudo ls -l $docker_daemon_conf_file
 echo 'restarting docker'
 sudo service docker restart
 systemctl status docker
-sudo systemctl restart docker
-systemctl status docker
+docker version -f '{{.Server.Experimental}}'
+echo "docker daemon config should be: `sudo cat $docker_daemon_conf_file || true`"
+
+#sudo systemctl restart docker
+#systemctl status docker
 
 echo 'checking for docker experimental...'
 docker version -f '{{.Server.Experimental}}'
