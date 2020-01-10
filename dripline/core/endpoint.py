9d3dd96a0f1aaed74ce4687b4694da328ab57de4
Ben LaRoque <benjamin.laroque@gmail.com>
2020-01-09 19:01:05 -0800

    adding more doc strings

--- dripline/core/endpoint.py
+++ dripline/core/endpoint.py
@@ -5,9 +5,15 @@ from _dripline.core import _Endpoint
 
 __all__.append('Endpoint')
 class Endpoint(_Endpoint):
-    def __init__(self, name, calibration=None):
+    '''
+    Base class for all objects which can be sent dripline requests.
+    Every object described in a runtime configuration passed to `dl-serve` should derive from this class (either directly or indirectly).
+    '''
+    def __init__(self, name):
+        '''
+        name (str) : the name of the endpoint, specifies the binding key for request messages to which this object should reply
+        '''
         _Endpoint.__init__(self, name)
-        self._calibration = calibration
 
     def do_get_request(self, a_request_message):
         print("in get_request")
--- dripline/core/throw_reply.py
+++ dripline/core/throw_reply.py
@@ -5,6 +5,19 @@ from _dripline.core import _ThrowReply, DL_Success, set_reply_cache
 
 __all__.append('ThrowReply')
 class ThrowReply(Exception):
+    '''
+    Exception class for use throughout the codebase for exceptions which do not fatally interrupt a running dripline service.
+    These exceptions are caught in the top of the message handling call stack and result in an reply message being sent indicating an error.
+    The preferred way to send a reply indicating an error (or warning) is to raise an instance of ThrowReply.
+    In cases where a dependency library throws a custom exception, or any piece of code throws a standard python exception, if the expected behavior is an error reply and continued service operation, that exception must be handled and ThrowReply raised instead.
+    '''
     def __init__(self, return_code=DL_Success(), message=DL_Success().description, payload=scarab.Param()):
+        '''
+        return_code (_dripline.core.ReturnCode) : instance of subclass of ReturnCode, it must be an instance which is already registered and is
+                                                  used to determine various header fields of the resulting dripline message.
+        message (string) : provides more specific information about the particular exception (beyond the fixed ReturnCode's description
+        payload (scarab.Param) : optional data to populate the ReplyMessage's payload, especially useful in cases of Warnings, where there is
+                                 potentially valid data to return in response to the request.
+        '''
         Exception.__init__(self)
         set_reply_cache(return_code, message, payload)
