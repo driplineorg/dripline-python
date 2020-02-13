2681324f9dd989d3bbe9278a043ecd2fd4c4dc65
yadiw <32943329+yadiw@users.noreply.github.com>
2020-02-13 10:28:41 -0800

    Update test_constants.py

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -1,4 +1,4 @@
-import dripline.core
+import scarab, _dripline.core
 
 OP_T_SET = 0
 OP_T_GET = 1
@@ -12,7 +12,7 @@ MSG_T_ALERT = 4
 MSG_T_UNKNOWN = 0xffffffff
     
 def test_op_t_to_uint():
-    item = dripline.core.op_t
+    item = _dripline.core.op_t
     assert(item.to_uint(item.set) == OP_T_SET)
     assert(item.to_uint(item.get) == OP_T_GET)
     assert(item.to_uint(item.run) == OP_T_RUN)
@@ -20,7 +20,7 @@ def test_op_t_to_uint():
     assert(item.to_uint(item.unknown) == OP_T_UKW)
 
 def test_op_t_to_string():
-    item = dripline.core.op_t
+    item = _dripline.core.op_t
     assert(item.to_string(item.set) == "set")
     assert(item.to_string(item.get) == "get")
     assert(item.to_string(item.run) == "run")
@@ -28,13 +28,13 @@ def test_op_t_to_string():
     assert(item.to_string(item.unknown) == "unknown")
     flag = False
     try:
-        item.to_string(dripline.core.msg_t.request)
+        item.to_string(_dripline.core.msg_t.request)
     except Exception:
         flag = True
     assert(flag)
 
 def test_op_t_uint_to_op_t():
-    item = dripline.core.op_t
+    item = _dripline.core.op_t
     assert(item.to_op_t(OP_T_SET) == item.set)
     assert(item.to_op_t(OP_T_GET) == item.get)
     assert(item.to_op_t(OP_T_RUN) == item.run)
@@ -42,7 +42,7 @@ def test_op_t_uint_to_op_t():
     assert(item.to_op_t(OP_T_UKW) == item.unknown)
 
 def test_op_t_string_to_op_t():
-    item = dripline.core.op_t
+    item = _dripline.core.op_t
     assert(item.to_op_t("set") == item.set)
     assert(item.to_op_t("get") == item.get)
     assert(item.to_op_t("run") == item.run)
@@ -56,34 +56,34 @@ def test_op_t_string_to_op_t():
     assert(flag)
     
 def test_msg_t_to_uint():
-    item = dripline.core.msg_t
+    item = _dripline.core.msg_t
     assert(item.to_uint(item.reply) == MSG_T_REPLY)
     assert(item.to_uint(item.request) == MSG_T_REQUEST)
     assert(item.to_uint(item.alert) == MSG_T_ALERT)
     assert(item.to_uint(item.unknown) == MSG_T_UNKNOWN)
 
 def test_msg_t_to_string():
-    item = dripline.core.msg_t
+    item = _dripline.core.msg_t
     assert(item.to_string(item.reply) == "reply")
     assert(item.to_string(item.request) == "request")
     assert(item.to_string(item.alert) == "alert")
     assert(item.to_string(item.unknown) == "unknown")
     flag = False
     try:
-        item.to_string(dripline.core.op_t.set)
+        item.to_string(_dripline.core.op_t.set)
     except Exception:
         flag = True
     assert(flag)
 
 def test_msg_t_uint_to_msg_t():
-    item = dripline.core.msg_t
+    item = _dripline.core.msg_t
     assert(item.to_msg_t(MSG_T_REPLY) == item.reply)
     assert(item.to_msg_t(MSG_T_REQUEST) == item.request)
     assert(item.to_msg_t(MSG_T_ALERT) == item.alert)
     assert(item.to_msg_t(MSG_T_UNKNOWN) == item.unknown)
 
 def test_msg_t_string_to_msg_t():
-    item = dripline.core.msg_t
+    item = _dripline.core.msg_t
     assert(item.to_msg_t("reply") == item.reply)
     assert(item.to_msg_t("request") == item.request)
     assert(item.to_msg_t("alert") == item.alert)
