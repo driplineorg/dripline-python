532885762f62e30e358e9b1becaadf80f4671992
yadiw <32943329+yadiw@users.noreply.github.com>
2020-02-13 10:25:43 -0800

    Update test_specifier.py

--- tests/test_specifier.py
+++ tests/test_specifier.py
@@ -1,16 +1,16 @@
-import dripline.core
+import scarab, _dripline.core
 
 def test_specifier_constructor():
     str_list = ["hi", "he-llo", ".", "1.", "5.6.7", ""]
     for s in str_list:
-        item = dripline.core.Specifier(s)
+        item = _dripline.core.Specifier(s)
         assert(item.to_string() == s)
-    item = dripline.core.Specifier()
+    item = _dripline.core.Specifier()
     assert(item.to_string() == "")
 
 def test_parse():
     str_list = ["hi", "he-llo", ".", "1.", "5.6.7", ""]
-    item = dripline.core.Specifier()
+    item = _dripline.core.Specifier()
     for s in str_list:
         item.parse(s)
         assert(item.to_string() == s)
@@ -18,7 +18,7 @@ def test_parse():
 
 def test_reparse():
     str_list = ["hi", "he-llo", ".", "1.", "5.6.7", ""]
-    item = dripline.core.Specifier()
+    item = _dripline.core.Specifier()
     item.reparse()
     assert(item.to_string() == "")
     for s in str_list:
