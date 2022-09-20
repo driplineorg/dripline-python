04cbc481fdcba8225fad5ac9b9265bbd66c91c53
Noah Oblath <noah.oblath@pnnl.gov>
2022-09-20 11:50:52 -0700

    Handle return-code conversion; specify that  host and port are for the HTTP server

--- dripline/implementations/http_server.py
+++ dripline/implementations/http_server.py
@@ -14,25 +14,61 @@ __all__ = []
 __all__.append('HTTPServer')
 class HTTPServer(Interface):
     '''
-    A fairly generic subclass of Service for connecting to ethernet-capable instruments/devices.
-    In is developed for and tested against devices with a SCPI-compliant command set, but may be usable with devices which do not strictly conform.
-    In particular, devices must support sending a response to every command received (either natively, or via SCPI's command composition) and responses are expected to include a termination marking complete transmission.
+    An HTTP-request server that acts as a Dripline client.
+    An HTTP client (e.g. user via a web browser) sends an HTTP request, which is converted to aDripline request, and forwarded to the Broker.
+    When the Dripline reply is received, that's converted to an HTTP response and returned to the HTTP client.
     '''
     def __init__(self,
-                 host='localhost',
-                 port=8080,
+                 http_host='localhost',
+                 http_port=8080,
                  **kwargs
                  ):
         '''
         Args:
-            host (string): IP address or URL for the HTTP server host
-            port (int): Port for the HTTP server host
+            http_host (string): IP address or URL for the HTTP server host
+            http_port (int): Port for the HTTP server host
 
         '''
         Interface.__init__(self, **kwargs)
 
-        self.host = '127.0.0.1' if host=='localhost' else host
-        self.port = port
+        self.http_host = '127.0.0.1' if http_host=='localhost' else http_host
+        self.http_port = http_port
+
+        self.ret_code_conversion = {
+            dripline.core.DL_Success.value: 200, # Success
+
+            dripline.core.DL_WarningNoActionTaken.value: 100, # Continue
+            dripline.core.DL_WarningDeprecatedFeature.value: 110, # Response is Stale
+            dripline.core.DL_WarningDryRun.value: 112, # Disconnected Operation
+            dripline.core.DL_WarningOffline.value: 112, # Disconnected Operation
+            dripline.core.DL_WarningSubService.value: 199, # Miscellaneous Warning
+
+            dripline.core.DL_AmqpError.value: 500, # Internal Server Error
+            dripline.core.DL_AmqpErrorBrokerConnection.value: 500, # Internal Server Error
+            dripline.core.DL_AmqpErrorRoutingkeyNotfound.value: 404, # Not found
+
+            dripline.core.DL_ResourceError.value: 500, # Internal Server Error
+            dripline.core.DL_ResourceErrorConnection.value: 503, # Service Unavailable
+            dripline.core.DL_ResourceErrorNoResponse.value: 503, # Service Unavailable
+            dripline.core.DL_ResourceErrorSubService.value: 503, # Service Unavailable
+
+            dripline.core.DL_ServiceError.value: 400, # Bad request
+            dripline.core.DL_ServiceErrorNoEncoding.value: 400, # Bad request
+            dripline.core.DL_ServiceErrorDecodingFail.value: 400, # Bad request
+            dripline.core.DL_ServiceErrorBadPayload.value: 422, # Unprocessable entity
+            dripline.core.DL_ServiceErrorInvalidValue.value: 422, # Unprocessable entity
+            dripline.core.DL_ServiceErrorTimeout.value: 503, # Service Unavailable
+            dripline.core.DL_ServiceErrorInvalidMethod.value: 501, # Not implemented
+            dripline.core.DL_ServiceErrorAccessDenied.value: 403, # Forbidden
+            dripline.core.DL_ServiceErrorInvalidKey.value: 423, # Locked
+            dripline.core.DL_ServiceErrorInvalidSpecifier.value: 422, # Unprocessable entity
+
+            dripline.core.DL_ClientError.value: 500, # Internal Server Error
+            dripline.core.DL_ClientErrorInvalidRequest.value: 400, # Bad Request
+            dripline.core.DL_ClientErrorHandlingReply.value: 500, # Internal Server Error
+            dripline.core.DL_ClientErrorUnableToSend.value: 503, # Service Unavailable
+            dripline.core.DL_ClientErrorTimeout.value: 504, # Gateway Timeout
+        }
 
     async def handle_get(self, request):
         routing_key = self.path_to_routing_key(request.path)
@@ -41,9 +77,12 @@ class HTTPServer(Interface):
         except Exception as err:
             return web.Response(text=f'Unable to send GET request: {err}')
         return web.Response(
-            #status=...,
+            status=self.ret_code_conversion.get(reply.return_code, 400),
             reason=reply.return_message,
-            body=json.dumps(scarab.to_python(reply.payload))
+            headers={
+                'dl_return_code': reply.return_code
+            },
+            body=json.dumps(scarab.to_python(reply.payload)),
         )
 
     async def handle_put(self, request):
@@ -56,14 +95,14 @@ class HTTPServer(Interface):
 
     def http_listen(self):
         '''
-        
+        Starts the HTTP server
         '''
         app = web.Application()
         app.router.add_route('GET', '/{tail:.*}', self.handle_get)
         app.router.add_route('PUT', '/{tail:.*}', self.handle_put)
         app.router.add_route('POST', '/{tail:.*}', self.handle_post)
 
-        web.run_app(app, host=self.host, port=self.port)
+        web.run_app(app, host=self.http_host, port=self.http_port)
 
     def path_to_routing_key(self, input:str):
         return input[1:].replace('/', '.')
