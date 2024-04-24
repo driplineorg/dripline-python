499690b031a315411eb55e033225405d6fe691eb
Noah Oblath <nsoblath@gmail.com>
2024-04-24 16:14:52 -0700

    Use docker healthcheck to determine when services should start; add simple-service to the integration testing

--- tests/integration/run-tests.sh
+++ tests/integration/run-tests.sh
@@ -1,5 +1,17 @@
 #! /usr/bin/env bats
 
+@test "ping simple" {
+    dl-agent -vv --auth-file /auths.json cmd simple -s ping
+}
+
+@test "get simple" {
+    dl-agent -vv --auth-file /auths.json get simple
+}
+
+@test "set simple" {
+    dl-agent -vv --auth-file /auths.json set simple 500
+}
+
 @test "ping my_store" {
     dl-agent -vv --auth-file /auths.json cmd my_store -s ping
 }
