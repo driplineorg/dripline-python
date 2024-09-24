45feba6e676c98368960d6db546c1ad06cd4a000
Noah Oblath <nsoblath@gmail.com>
2024-09-24 11:05:18 -0700

    Change lockout_key binding in MsgRequest to use the python uuid package instead of strings

--- module_bindings/dripline_core/message_pybind.hh
+++ module_bindings/dripline_core/message_pybind.hh
@@ -6,13 +6,17 @@
 #include "message.hh"
 #include "message_trampoline.hh"
 
+#include "uuid.hh"
+
+#include "logger.hh"
+
 #include "pybind11/pybind11.h"
 #include "pybind11/iostream.h"
 #include "pybind11/eval.h"
 
-#include "uuid.hh"
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/lexical_cast.hpp>
 
-#include "logger.hh"
 LOGGER( dlog_mph, "message_pybind.hh" )
 
 namespace dripline_pybind
@@ -97,24 +101,18 @@ namespace dripline_pybind
 
             // properties
             // mv_referrable
-            //TODO even better if we could interface with python's UUID library instead of passing strings
-            //py::object result = calc.attr("add")(1, 2);
-            /*.def_property( "lockout_key",
-                           [](dripline::request_ptr_t a_req){ return boost::uuids::to_string(a_req->lockout_key()); },
-                           [](dripline::request_ptr_t a_req, const std::string& a_lockout_key ){
-                                bool t_lockout_key_valid = true;
-                                a_req->lockout_key() = dripline::uuid_from_string( a_lockout_key, t_lockout_key_valid );
-                                a_req->set_lockout_key_valid( t_lockout_key_valid );
-                             }
-                         )*/
             .def_property( "lockout_key",
-                           [py_uuid](dripline::request_ptr_t a_req){ return py_uuid.attr("UUID")("bytes"_a=a_req->lockout_key()); },
-                           [py_uuid](dripline::request_ptr_t a_req, void* a_lockout_key ){
-                                memcpy( &a_req->lockout_key(), a_lockout_key, 16 );
-                                //bool t_lockout_key_valid = true;
-                                //a_req->lockout_key() = dripline::uuid_from_string( a_lockout_key, t_lockout_key_valid );
-                                //a_req->set_lockout_key_valid( t_lockout_key_valid );
-                             }
+                           [py_uuid](const dripline::request_ptr_t a_req){
+                                std::stringstream str_strm;
+                                str_strm << a_req->lockout_key();
+                                return py_uuid.attr("UUID")(str_strm.str());
+                           },
+                           [py_uuid](dripline::request_ptr_t a_req, const pybind11::object& a_uuid ){
+                                std::stringstream str_strm;
+                                str_strm << pybind11::str(a_uuid);
+                                a_req->lockout_key() = boost::lexical_cast<dripline::uuid_t>(str_strm.str());
+                           },
+                           pybind11::return_value_policy::move
                          )
             // mv_accessible
             .def_property( "lockout_key_valid", &dripline::msg_request::get_lockout_key_valid, &dripline::msg_request::set_lockout_key_valid )
