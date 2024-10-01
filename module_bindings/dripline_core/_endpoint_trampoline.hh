2a0524c99b81e20b41ea37e19eb2147daf3be0dd
Noah Oblath <nsoblath@gmail.com>
2024-10-01 16:54:29 -0700

    Adapt to modifications in dl-cpp: service now has run()

--- module_bindings/dripline_core/_endpoint_trampoline.hh
+++ module_bindings/dripline_core/_endpoint_trampoline.hh
@@ -2,6 +2,7 @@
 #define DRIPLINE_PYBIND_ENDPOINT_TRAMPOLINE
 
 #include "endpoint.hh"
+
 #include "pybind11/pybind11.h"
 
 namespace dripline_pybind
@@ -16,35 +17,35 @@ namespace dripline_pybind
             // Overrides for virtual on_[messgate-type]_message()
             dripline::reply_ptr_t on_request_message( const dripline::request_ptr_t a_request ) override
             {
-                pybind11::gil_scoped_acquire t_acquire;
-                PYBIND11_OVERLOAD( dripline::reply_ptr_t, dripline::endpoint, on_request_message, a_request );
+                //pybind11::gil_scoped_acquire t_acquire;
+                PYBIND11_OVERRIDE( dripline::reply_ptr_t, dripline::endpoint, on_request_message, a_request );
             }
             void on_reply_message( const dripline::reply_ptr_t a_reply ) override
             {
-                pybind11::gil_scoped_acquire t_acquire;
-                PYBIND11_OVERLOAD( void, dripline::endpoint, on_reply_message, a_reply );
+                //pybind11::gil_scoped_acquire t_acquire;
+                PYBIND11_OVERRIDE( void, dripline::endpoint, on_reply_message, a_reply );
             }
             void on_alert_message( const dripline::alert_ptr_t a_alert ) override
             {
-                pybind11::gil_scoped_acquire t_acquire;
-                PYBIND11_OVERLOAD( void, dripline::endpoint, on_alert_message, a_alert );
+                //pybind11::gil_scoped_acquire t_acquire;
+                PYBIND11_OVERRIDE( void, dripline::endpoint, on_alert_message, a_alert );
             }
 
             // Overrides for virtual do_[request-type]_request
             dripline::reply_ptr_t do_get_request( const dripline::request_ptr_t a_request ) override
             {
-                pybind11::gil_scoped_acquire t_acquire;
-                PYBIND11_OVERLOAD( dripline::reply_ptr_t, dripline::endpoint, do_get_request, a_request );
+                //pybind11::gil_scoped_acquire t_acquire;
+                PYBIND11_OVERRIDE( dripline::reply_ptr_t, dripline::endpoint, do_get_request, a_request );
             }
             dripline::reply_ptr_t do_set_request( const dripline::request_ptr_t a_request ) override
             {
-                pybind11::gil_scoped_acquire t_acquire;
-                PYBIND11_OVERLOAD( dripline::reply_ptr_t, dripline::endpoint, do_set_request, a_request );
+                //pybind11::gil_scoped_acquire t_acquire;
+                PYBIND11_OVERRIDE( dripline::reply_ptr_t, dripline::endpoint, do_set_request, a_request );
             }
             dripline::reply_ptr_t do_cmd_request( const dripline::request_ptr_t a_request ) override
             {
-                pybind11::gil_scoped_acquire t_acquire;
-                PYBIND11_OVERLOAD( dripline::reply_ptr_t, dripline::endpoint, do_cmd_request, a_request );
+                //pybind11::gil_scoped_acquire t_acquire;
+                PYBIND11_OVERRIDE( dripline::reply_ptr_t, dripline::endpoint, do_cmd_request, a_request );
             }
     };
 
