f85651207f0f10929535afdb64592d7503ce2a17
Noah Oblath <nsoblath@gmail.com>
2024-11-21 17:25:51 -0800

    Make object_creator a bit more chatty about what types of objects it's creating

--- dripline/core/object_creator.py
+++ dripline/core/object_creator.py
@@ -41,6 +41,7 @@ class ObjectCreator:
         extra_namespace = object()
         if module_path:
             try:
+                logger.debug(f'Loading module path: {module_path}')
                 spec = importlib.util.spec_from_file_location('extra_namespace', module_path)
                 extra_namespace = importlib.util.module_from_spec(spec)
                 spec.loader.exec_module(extra_namespace)
@@ -52,23 +53,28 @@ class ObjectCreator:
         #first priority, if it is in the module_path provided (if any)
         if hasattr(extra_namespace, module_name):
             module = getattr(extra_namespace, module_name)
+            logger.debug(f'Found module {module_name} in provided path')
         #second priority, if it is in any extensions installed
         elif hasattr(dripline.extensions, module_name):
             module = getattr(dripline.extensions, module_name)
+            logger.debug(f'Found module {module_name} in dripline.extensions')
         ## Note: we only loop one layer down in the config file, this could possibly be made more generic
         else:
             for m in [getattr(dripline.extensions, i_mod) for i_mod in dir(dripline.extensions)]:
                 if inspect.ismodule(m):
                     if hasattr(m, module_name):
                         module = getattr(m, module_name)
+                        logger.debug(f'Found module {module_name} in dripline.extensions.{m.__name__}')
 
         if module is None: # continue above elifs
         #third priority, if it is part of the base set of implementations
             if hasattr(dripline.implementations, module_name):
                 module = getattr(dripline.implementations, module_name)
+                logger.debug(f'Found module {module_name} in dripline.implementations')
         #fourth priority, if it is in core
             elif hasattr(dripline.core, module_name):
                 module = getattr(dripline.core, module_name)
+                logger.debug(f'Found module {module_name} in dripline.core')
             else:
                 raise NameError('no module "{}" in available namespaces'.format(module_name))
             
@@ -77,5 +83,6 @@ class ObjectCreator:
             the_object = module( authentication_obj=auth, **config )
         else:
             the_object = module( **config )
+        logger.debug(f'Object type: {type(the_object)}')
 
         return the_object
