51d82891e4b963f73b38d12bcb97cf536dd6c467
wcpettus <wcpettus@gmail.com>
2026-03-04 17:30:52 -0500

    Add new PostgresMappedSensorLogger class to handle mapping database structures, fix mutable defaults in postgres classes

--- dripline/implementations/postgres_interface.py
+++ dripline/implementations/postgres_interface.py
@@ -76,19 +76,20 @@ class SQLTable(Endpoint):
     '''
     A class for making calls to _insert_with_return
     '''
-    def __init__(self, table_name,
+    def __init__(self,
+                 table_name,
                  schema=None,
-                 required_insert_names=[],
-                 return_col_names=[],
-                 optional_insert_names=[],
-                 default_insert_values={},
+                 required_insert_names=(),
+                 insert_return_col_names=(),
+                 optional_insert_names=(),
+                 default_insert_values=None,
                  *args,
-                **kwargs):
+                 **kwargs):
         '''
         table_name (str): name of the table within the database
         schema (str): name of the schema where the table is located
         required_insert_names (list): list of names (str||dict) of the table columns which must be included on every requested insert (if dict: keys are 'column' and 'payload_key', if string it is assumed that both are that value)
-        return_col_names (list): list of names (str) of columns whose values should be returned on completion of the insert
+        insert_return_col_names (list): list of names (str) of columns whose values should be returned on completion of the insert
         optional_insert_names (list): list of names (str||dict) of columns which the user may specify on an insert request, but which may be omitted (if dict: keys are 'column' and 'payload_key', if string it is assumed that both are that value)
         default_insert_values (dict): dictionary of {column_names: values} to serve as defaults when inserting, any values provided explicitly on the insert request will override these values
         '''
@@ -98,11 +99,14 @@ class SQLTable(Endpoint):
         self.table = None
         self.table_name = table_name
         self.schema = schema
-        self._return_names = return_col_names
+        self._return_names = insert_return_col_names
         self._column_map = {}
         self._required_insert_names = self._ensure_col_key_map(required_insert_names)
         self._optional_insert_names = self._ensure_col_key_map(optional_insert_names)
-        self._default_insert_dict = default_insert_values
+        if default_insert_values is None:
+            self._default_insert_dict = {}
+        else:
+            self._default_insert_dict = default_insert_values
 
     def _ensure_col_key_map(self, column_list):
         to_return = []
@@ -111,7 +115,7 @@ class SQLTable(Endpoint):
                 to_return.append({'column': a_col, 'payload_key': a_col})
                 self._column_map[a_col] = a_col
             elif isinstance(a_col, dict):
-                if not 'column' in a_col or not 'payload_key' in a_col:
+                if not 'column' in a_col and not 'payload_key' in a_col:
                     raise KeyError(f"column insert map <{a_col}> does not contain the required keys, ['column', 'payload_key']")
                 to_return.append(a_col)
                 self._column_map[a_col['payload_key']] = a_col['column']
@@ -119,7 +123,7 @@ class SQLTable(Endpoint):
                 raise TypeError(f"column info <{a_col}> is not of an expected type")
         return to_return
 
-    def do_select(self, return_cols=[], where_eq_dict={}, where_lt_dict={}, where_gt_dict={}):
+    def do_select(self, return_cols=(), where_eq_dict=None, where_lt_dict=None, where_gt_dict=None):
         '''
         return_cols (list of str): string names of columns, internally converted to sql reference; if evaluates as false, all columns are returned
         where_eq_dict (dict): keys are column names (str), and values are tested with '=='
@@ -134,12 +138,15 @@ class SQLTable(Endpoint):
             this_select = sqlalchemy.select(self.table)
         else:
             this_select = sqlalchemy.select(*[getattr(self.table.c,col) for col in return_cols])
-        for c,v in where_eq_dict.items():
-            this_select = this_select.where(getattr(self.table.c,c)==v)
-        for c,v in where_lt_dict.items():
-            this_select = this_select.where(getattr(self.table.c,c)<v)
-        for c,v in where_gt_dict.items():
-            this_select = this_select.where(getattr(self.table.c,c)>v)
+        if where_eq_dict is not None:
+            for c,v in where_eq_dict.items():
+                this_select = this_select.where(getattr(self.table.c,c)==v)
+        if where_lt_dict is not None:
+            for c,v in where_lt_dict.items():
+                this_select = this_select.where(getattr(self.table.c,c)<v)
+        if where_gt_dict is not None:
+            for c,v in where_gt_dict.items():
+                this_select = this_select.where(getattr(self.table.c,c)>v)
         with self.service.engine.connect() as conn:
             result = conn.execute(this_select)
         return (result.keys(), [i for i in result])
@@ -157,7 +164,7 @@ class SQLTable(Endpoint):
             return_values = []
         return dict(zip(return_col_names_list, return_values))
 
-    def do_insert(self, *args, **kwargs):
+    def do_insert(self, **kwargs):
         '''
         '''
         # make sure that all provided insert values are expected
